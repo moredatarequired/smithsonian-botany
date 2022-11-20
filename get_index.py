9f658882a652e2591f02c7a183ff86a53cc83f3f
Hugh Wimberly <hugh.wimberly@gmail.com>
2022-11-20 12:09:19 -0800

    Cache actual dataframe to Parquet

--- get_index.py
+++ get_index.py
@@ -8,14 +8,15 @@ import pandas as pd
 import requests
 from tqdm import tqdm
 
+# See https://registry.opendata.aws/smithsonian-open-access/
 root_url = "https://smithsonian-open-access.s3-us-west-2.amazonaws.com/metadata/edan/"
 head_index = root_url + "index.txt"
 
 
-def get_with_progress_bar(url):
+def get_with_progress_bar_and_cache(url):
     assert url.startswith(root_url)
     path = url[len(root_url) :]
-    dest_file = Path(".") / Path("cache") / path
+    dest_file = Path(".") / "cache" / "web" / path
 
     response = requests.get(url, stream=True, allow_redirects=True)
     file_size = int(response.headers.get("Content-Length", 0))
@@ -37,7 +38,18 @@ def get_with_progress_bar(url):
 
 
 def get_values(index_url):
-    return get_with_progress_bar(index_url).splitlines()
+    return get_with_progress_bar_and_cache(index_url).splitlines()
+
+
+def dataframe_from_json_url(url):
+    tag = Path(urlparse(url).path).stem
+    df_cache = Path(".") / "cache" / "dataframes" / f"{tag}.parquet"
+    if not df_cache.exists():
+        df_cache.parent.mkdir(exist_ok=True, parents=True)
+        json_lines = get_values(url)
+        df = pd.DataFrame.from_dict([json.loads(j) for j in json_lines])
+        df.to_parquet(df_cache)
+    return pd.read_parquet(df_cache)
 
 
 def main():
@@ -46,11 +58,10 @@ def main():
 
     botany_index = get_values(botany)
     botany_metadata = []
-    for i in tqdm(botany_index):
-        json_lines = get_values(i)
-        botany_metadata.extend([json.loads(j) for j in json_lines])
+    for subindex in tqdm(botany_index):
+        botany_metadata.append(dataframe_from_json_url(subindex))
 
-    df = pd.DataFrame.from_dict(botany_metadata)
+    df = pd.concat(botany_metadata)
     print(df.describe())
 
 
